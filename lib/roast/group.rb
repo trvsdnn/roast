89803cabb2a2abe702df4ae74c8eebccd6c87f6f
blahed <tdunn13@gmail.com>
2013-03-09 17:37:24 -0500

    add find_host method to group

--- lib/roast/group.rb
+++ lib/roast/group.rb
@@ -1,7 +1,6 @@
 module Roast
   class Group
     attr_reader :name
-    attr_reader :hosts
 
     def initialize(name)
       @name     = name
@@ -9,34 +8,46 @@ module Roast
     end
 
     def disable!
-      hosts.values.each { |h| h.disable! }
+      hosts.each { |h| h.disable! }
     end
 
     def enable!
-      hosts.values.each { |h| h.enable! }
+      hosts.each { |h| h.enable! }
     end
 
     def disabled?
-      hosts.values.all? { |h| h.disabled? }
+      hosts.all? { |h| h.disabled? }
     end
 
     def enabled?
-      hosts.values.all? { |h| h.enabled? }
+      hosts.all? { |h| h.enabled? }
+    end
+
+    def hosts
+      @hosts.values
     end
 
     def <<(host)
-      hosts[host.hostname] = host
+      @hosts[host.hostname] = host
     end
 
     def [](hostname)
-      hosts[hostname]
+      @hosts[hostname]
+    end
+
+    def find_host(entry)
+      if entry =~ Host::IP_PATTERN
+        hosts.select { |h| h.ip_address == entry }
+      else
+        hosts.select { |h| h.hostname == entry }
+      end
     end
 
     def to_cli
       string = " - \033[4m#{name}\033[0m\n"
-      max    = hosts.values.map { |h| h.hostname.size }.max
+      max    = hosts.map { |h| h.hostname.size }.max
 
-      hosts.values.each do |host|
+      hosts.each do |host|
         padding = ' ' * (max - host.hostname.size + 4)
         if host.disabled?
           string << "   \033[31m \u00D7 "
@@ -50,10 +61,10 @@ module Roast
     end
 
     def to_hosts_file
-      max     = hosts.values.map { |h| h.ip_address.size }.max
+      max     = hosts.map { |h| h.ip_address.size }.max
       section = "## ROAST [#{name}]\n"
 
-      hosts.values.each do |host|
+      hosts.each do |host|
         padding = ' ' * (max - host.ip_address.size + 4)
         section << '# ' if host.disabled?
         section << "#{host.ip_address}#{padding}#{host.hostname}\n"
--- test/group_test.rb
+++ test/group_test.rb
@@ -11,17 +11,36 @@ describe Roast::Group do
   it "adds a host to the group" do
     host = Roast::Host.new('127.0.0.1', 'something.dev')
     @group << host
-    @group.hosts.has_key?(host.hostname).must_equal true
+    @group.hosts.include?(host).must_equal true
   end
 
   it "disables and enables all hosts in the group" do
     @group.disable!
     @group.must_be :disabled?
-    @group.hosts.values.all? { |h| h.disabled? }.must_equal true
+    @group.hosts.all? { |h| h.disabled? }.must_equal true
 
     @group.enable!
     @group.must_be :enabled?
-    @group.hosts.values.all? { |h| h.enabled? }.must_equal true
+    @group.hosts.all? { |h| h.enabled? }.must_equal true
+  end
+
+  it "finds hosts by ip address" do
+    results = @group.find_host('127.0.0.1')
+    results.wont_be :empty?
+    results.size.must_equal 1
+    results.first.must_equal @group.hosts.first
+  end
+
+  it "finds hosts by hostname" do
+    results = @group.find_host('example.org')
+    results.wont_be :empty?
+    results.size.must_equal 1
+    results.first.must_equal @group.hosts.last
+  end
+
+  it "returns an empty array if host could not be found" do
+    results = @group.find_host('something.else')
+    results.must_be :empty?
   end
 
   it "outputs host entries to cli correctly" do
