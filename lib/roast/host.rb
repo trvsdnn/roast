cae0d1b60c125cb9c3b665b9eb6c19e30a2aec6d
blahed <tdunn13@gmail.com>
2013-03-08 22:51:34 -0500

    add ip/host validation

--- lib/roast/host.rb
+++ lib/roast/host.rb
@@ -1,13 +1,21 @@
 module Roast
   class Host
+    IP_PATTERN = /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/
+    HOST_PATTERN = /\A[a-z0-9\-\.]+\z/
     IP_HOST_PATTERN = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+([^\s]+)/
 
     attr_reader :ip_address
     attr_reader :hostname
 
     def initialize(ip_address, hostname)
-      @ip_address = ip_address
-      @hostname   = hostname
+      @ip_address = ip_address.chomp
+      @hostname   = hostname.chomp.downcase
+      validate!
+    end
+
+    def validate!
+      raise ArgumentError, "`#{ip_address}' is an invalid ip address" unless ip_address =~ IP_PATTERN
+      raise ArgumentError, "`#{hostname}' is an invalid hostname" unless hostname =~ HOST_PATTERN
     end
 
     def self.parse_and_create(line)
--- test/host_test.rb
+++ test/host_test.rb
@@ -8,5 +8,17 @@ describe Roast::Host do
     host.hostname.must_equal 'foobar.dev'
   end
 
+  it "validates the ip address" do
+    ['foo', 'example.com', '127.0.0.1.1', '1277.0.0.1'].each do |ip|
+      lambda { Roast::Host.new(ip, 'blah.dev') }.must_raise ArgumentError
+    end
+  end
+
+  it "validates the hostname" do
+    ['foo_com', '@#$asdf.com'].each do |hostname|
+      lambda { Roast::Host.new('127.0.0.1', hostname) }.must_raise ArgumentError
+    end
+  end
+
 
 end
